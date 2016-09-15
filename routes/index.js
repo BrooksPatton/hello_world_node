9155cf978027a93b6de2ab5bdbd999080a37d503
Brooks Patton <brooks@brookzerker.com>
2016-09-14 21:05:37 -0600

    removed console.logs

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,6 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   Todo.getAll().then((data) => {
-    console.log(data);
     res.render('index', { todos: data });
   });
 });
